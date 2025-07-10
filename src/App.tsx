1895501e4c042174ea75c8de8ba6c4e42f4857d8
Gabriel Torres <gabrielmksm1900@outlook.com>
2025-07-10 18:26:45 -0300

    alterações no layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading'
+//import { Heading } from './components/Heading'
 import { TimerIcon } from 'lucide-react'
+
+import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
 
 export function App() {
     return (
         <>
-            <div className='container'>
-                <div className='content'>
-                    <section>MENU</section>
-                </div>
-            </div>
+            <Container>
+                <Logo />
+            </Container>
+
+            <Container>
+                <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className='form' action=''>
+                    <div>
+                        <label htmlFor="task">What do you want to work on?</label>
+                        <input type="text" id="task" placeholder="Name of the task" />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, deleniti 
+                            esse illo doloribus incidunt reiciendis magni quo accusantium maiores nisi 
+                            natus voluptate culpa ipsum distinctio, quia, nesciunt vero laboriosam voluptatem!
+                        </p>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </Container>
+            
         </>
     )
 }
